648eeb8edd2d2eaf454023ef604d8c2715557936
Seth Dominion <sethdominion@Seths-MacBook-Pro.local>
2023-02-14 18:37:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SERVIDOR DE NODE
+
+NodeJS con Express
---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+    res.send('Hola mundo!')
+})
+app.listen(3000)
