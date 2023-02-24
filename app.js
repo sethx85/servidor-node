16a160a02c011566135d78a99e08e35a2b28fe8f
Seth Dominion <sethdominion@Seths-MacBook-Pro.local>
2023-02-23 20:10:40 -0500

    2nd commit

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 const express = require('express')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+
 const app = express()
 
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', function(req, res) {
-    res.send('Hola mundo!')
+app.use(cookieParser('mi ultra secreto'))
+
+app.use(session({
+    secret: 'mi ultra secreto',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+ app.set('view engine', 'ejs')
+
+app.get('/',(req, res)=>{
+    // Si ya iniciamos mostrar bienvenida
+
+
+    // Si no hemos iniciado sesion redireccionar a /login
+
 })
 
-app.post('/imagen', function (req, res) {
-    const body = req.body
-    console.log(body)
-    res.send('Hola Mundo desde el POST')
+app.get('/login',(req,res)=>{
+    // Mostrar el formulario de login
+    res.render('login')
 })
 
-app.listen(3000)
+
+app.post('/login',(req, res)=>{
+    // Recibir credenciales e iniciar sesion
+    
+})
+
+app.listen(8080,()=> console.log("Server started"))
