041588fd3bb6ade4cbbfca8250f1e25232511c52
Seth Dominion <sethdominion@Seths-MacBook-Pro.local>
2023-02-23 20:09:23 -0500

    2nd commit

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', function(req, res) {
     res.send('Hola mundo!')
 })
+
+app.post('/imagen', function (req, res) {
+    const body = req.body
+    console.log(body)
+    res.send('Hola Mundo desde el POST')
+})
+
 app.listen(3000)
